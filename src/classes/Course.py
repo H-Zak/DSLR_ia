1ce191bc0cf39a77cb037141d49a51201fd58a55
Daniel alejandro NIETO CASTRO <dnieto-c@e2r4p10.clusters.42paris.fr>
2024-08-29 16:58:12 +0200

    [TODO] Make a interactive menu for choose Hogwarts house and course

--- src/classes/Course.py
+++ src/classes/Course.py
@@ -11,17 +11,19 @@ class Course:
         self.raw_data = raw_data
         self.data_sorted = data_sorted
         self.count = len(self.data_sorted)
-        self.mean = 0
-        self.std = 0
+        self.mean = self.mean_calcul()
+        self.std = self.varirance()
         self.min = self.init_min_value() 
-        self.percentile_25 = self.get_percentile(0.25)
-        self.percentile_50 = self.get_percentile(0.5)
-        self.percentile_75 = self.get_percentile(0.75)
+        self.percentile_25 = self.init_percentile(0.25)
+        self.percentile_50 = self.init_percentile(0.5)
+        self.percentile_75 = self.init_percentile(0.75)
         self.max = self.init_max_value()
     
     def __str__(self) -> str:
         return (f'Course: {self.name}\n'
                 f'  Count:     {self.count}\n'
+                f'  Mean:      {self.mean}\n'
+                f'  Std:       {self.std}\n'
                 f'  Min Score: {self.min}\n'
                 f'  25%      : {self.percentile_25}\n'
                 f'  50%      : {self.percentile_50}\n'
@@ -35,34 +37,39 @@ class Course:
         return round(self.data_sorted[len(self.data_sorted) - 1], 4)
 
     def init_percentile(self, divisor):
-        count = self.count + 1
+        count = self.count - 1
         index = count * divisor
         percentile = 0
-        print(index)
+        percentage = index - int(index)
         if index % 2 != 0:
             index = int(index)
-            percentile = (self.data_sorted[index] + self.data_sorted[index + 1]) / 2
+            percentile = (self.data_sorted[index] + percentage * (self.data_sorted[index + 1] - self.data_sorted[index]))
         else:
             index = int(index)
             percentile = self.data_sorted[index]
-
         return round(percentile, 4)
 
-    def get_percentile(self, divisor):
-        print('---------------------------')
-        print(self.count)
-        index = math.ceil(divisor * (self.count))
-        index_2 = index - 1
-        print(index)
-        print(index_2)
-        return self.data_sorted[index]
-
-
-    # def init_percentile_25(self):
-    #     return self.data_sorted[0]
+    def sum(self):
+        sum = 0
+        for value in self.data_sorted:
+            sum += value 
+        return sum
     
-    # def init_percentile_50(self):
-    #     return self.data_sorted[0]
+    def mean_calcul(self):
+        my_sum = self.sum()
+        if self.count != 0:
+            return round(my_sum / self.count, 4)
+        else:
+             return None
     
-    # def init_(self):
-    #     return self.data_sorted[0]
+    def varirance(self):
+        variance = 0
+        for value in self.data_sorted:
+            variance += (value - self.mean) ** 2
+        if self.count == 0:
+            return None
+        variance = np.sqrt(variance/self.count)
+        return round(variance, 4)
+    
+    def get_name(self):
+        return self.name
---
+++ src/scatter_plot.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import numpy as np
+import math
+
+from typing import Union, List
+
+# Import course
+from classes.Course import Course
+# Import algo for sort
+from modules.quick_sort import quick_sort
+
+def sort_column(df : pd.DataFrame, name_column : str) -> np.ndarray:
+    column_data = df[name_column].values
+    # Select all not Nan values
+    column_with_no_nan = [num for num in column_data if not math.isnan(num)]
+    # Sorting values
+    column_data_sorted = quick_sort(column_with_no_nan)
+
+    return column_data_sorted
+
+
+def get_tab_courses(df : pd.DataFrame) -> List[Course]:
+    courses : List[Course] = []
+    # Getting all the columns with numeric values
+    numeric_df = df.select_dtypes(include=np.number)
+    
+    # Getting name of the numeric columns
+    list_courses = [column for column in numeric_df.columns if column != "Index"]
+    # Sorting data
+    for course in list_courses:
+        sort_data_course = sort_column(numeric_df, course)
+        new_course = Course(course, numeric_df[course], sort_data_course)
+        courses.append(new_course)
+
+    return courses
+
+def print_data_house_courses(house_courses : dict):
+    
+    for house in house_courses:
+        print("******************************")
+        print(house)
+        print("******************************")
+        for course in house_courses[house]:
+            print(course)
+
+def get_course_info_from_house(house_courses : dict, house_searched : str, course_searched : str):
+    
+    if house_searched in house_courses:
+        for course  in house_courses[house_searched]:
+            if course.get_name() == course_searched:
+                print('//////////////////////')
+                print(course)
+                print('//////////////////////')
+
+def describe_data_house(path_to_data_file : str):
+    # Reading data
+    df = pd.read_csv(path_to_data_file)
+    # Getting Hogwarts's House
+    houses = df['Hogwarts House'].unique()
+    # Dict {'House' : List[Course]}
+    house_courses = {}
+    for house in houses:
+        house_df = df[df['Hogwarts House'] == house]
+        house_courses[house] = get_tab_courses(house_df)
+
+    # print_data_house_courses(house_courses)
+
+    get_course_info_from_house(house_courses, houses[0], 'Arithmancy')
+
+def describe_data(path_to_data_file : str):
+    # Reading data
+    df = pd.read_csv(path_to_data_file)
+
+    courses_data = get_tab_courses(df)
+
+    for course in courses_data:
+        print(course)
+
+def main():
+    try:
+        describe_data_house('../datasets/dataset_train.csv')
+    except FileNotFoundError:
+        print('Failed to read the dataset')
+
+
+if __name__ == "__main__":
+    main()
