12c9389b7040e6f52f088ed3dab7ef9973c27cd5
Zakariya HAMDOUCHI <zhamdouc@paul-f4Ar3s1.clusters.42paris.fr>
2024-09-13 18:19:11 +0200

    nearly finish interface for training, issue with nan, count is not full for all course

--- src/data_visualization/training_board.py
+++ src/data_visualization/training_board.py
@@ -3,6 +3,7 @@ import pandas as pd
 import pandas as pd
 import numpy as np
 from typing import List
+import pprint
 
 import pandas as pd
 import time
@@ -114,63 +115,167 @@ def generate_all_the_scatter_plots_of_grades(house_classes_data : dict, list_cou
 # lancer le training 
 #enregistrer dans un dossier les listes avec avec un taux de reussite elever, avec le nom des listes et le pourcentage de resultat
 
+# Gryffindor = 1
+# Slytherin = 2
+# Ravenclaw = 3
+# Hufflepuff = 4
+
+
+
+#mettre a la place de raw_data, un raw data zscore
+def prepare_data(chosen_data, data):
+    print("start preparation of data")
+    courses = []
+    result = Course('Hogwarts House', data['Hogwarts House'].dropna().tolist())
+    # print(result.raw_data)
+    # courses.append(Course('Hogwarts House', data['Hogwarts House'].dropna().tolist()))
+    for course in chosen_data:
+        if '/' in course:
+            print("combinaison")
+            parts = course.split('/')
+            combined_data = []
+            for part in parts:
+                course_data = data[part].dropna().tolist()
+                combined_data.append(course_data)
+            combined_mean_data = np.mean(combined_data, axis=0).tolist()
+            courses.append(Course(course, combined_mean_data, True))
+        else:
+            print("not combinaison")
+            course_data = data[course].dropna().tolist()
+            courses.append(Course(course, course_data, True))
+    for course in courses:
+        print(course.name)
+    df_courses = pd.DataFrame()
+
+    for course in courses:
+        df_courses[course.name] = pd.Series(course.data_sorted)
+        print("here 1" ,len(course.data_sorted), len(course.raw_data), course.count)
+        print("HERE", len(df_courses[course.name]))
+    num_rows = len(df_courses)
+    num_cols = len(courses) + 1 #car il a Hogwarts House qui n'est pas un cours, donc pas besoin de faire +1
+    print(num_rows,num_cols)
+    input_matrix = np.ones((num_rows, num_cols))
+
+    for i, course in enumerate(courses):
+        input_matrix[:, i + 1] = df_courses[course.name].values
+    output_gryffindor = np.zeros((num_rows, 1))
+    output_slytherin = np.zeros((num_rows, 1))
+    output_ravenclaw = np.zeros((num_rows, 1))
+    output_hufflepuff = np.zeros((num_rows, 1))
+    print("longeur des resultat",len(result.data_sorted))
+    for i, house in enumerate(result.data_sorted):
+        if house == 1:  # Gryffindor
+            output_gryffindor[i] = 1
+        elif house == 2:  # Slytherin
+            output_slytherin[i] = 1
+        elif house == 3:  # Ravenclaw
+            output_ravenclaw[i] = 1
+        elif house == 4:  # Hufflepuff
+            output_hufflepuff[i] = 1
+        
+    theta = np.zeros((num_cols, 1))
+    print("input data:\n", input_matrix)
+    # print("Output Gryffindor:\n", output_gryffindor)
+    # print("Output Slytherin:\n", output_slytherin)
+    # print("Output Ravenclaw:\n", output_ravenclaw)
+    # print("Output Hufflepuff:\n", output_hufflepuff)
+    # print("Theta:\n", theta)
+    
+    # matrix = df_courses.to_numpy()
 
 def main():
-	try:
-		# Reading data
-		df = pd.read_csv('../datasets/dataset_train.csv')
+    try:
+        # Reading data
+        df = pd.read_csv('../datasets/dataset_train.csv')
 
-		# Getting all the columns with numeric values
-		numeric_df = select_numeric_columns(df)
-		# Getting names of the numeric columns
-		list_courses = [column for column in numeric_df.columns if column != "Index"]
+        # Get name of Hogwarts classes
+        unique_houses = df['Hogwarts House'].unique()
+            # Dictionnaire de correspondance
+        house_mapping = {
+            'Gryffindor': 1,
+            'Slytherin': 2,
+            'Ravenclaw': 3,
+            'Hufflepuff': 4
+        }
 
-		# Get name of Hogwarts classes
-		unique_houses = df['Hogwarts House'].unique()
+        # Appliquer la correspondance
+        df['Hogwarts House'] = df['Hogwarts House'].map(house_mapping)
+        # Getting all the columns with numeric values
+        numeric_df = select_numeric_columns(df)
+        # Getting names of the numeric columns
+        list_courses = [column for column in numeric_df.columns if column != "Index"]
+        # print(x for x in list(list_courses))
 
-		# Getting main data structure
-		house_classes_data = create_houses_list_of_course(df, unique_houses, list_courses)
+        # Getting main data structure
+        # house_classes_data = create_houses_list_of_course(df, unique_houses, list_courses)
 
-		feature : str = ''
-		first_class : str = ''
-		second_class : str = ''
-		normalized_data_flag : bool = False
-		list_features : list =  [unique_houses]
-		while True:
-			feature = curses.wrapper(lambda stdscr: prompt(stdscr, list_features, "Choose feature class:\n", False))
-			if feature == 'EXIT':
-				return
-			
-			if feature == 'all_scatter_plots_grades':
-				generate_all_the_scatter_plots_of_grades(house_classes_data, list_courses)
-				exit()
+        feature : str = ''
+        first_class : str = ''
+        second_class : str = ''
+        normalized_data_flag : bool = False
+        list_features : list =  [x for x  in list_courses if x != 'Hogwarts House']
+        list_features.extend(['Combinaison', 'Finish'])
+        # print(list_features)
+        chosen_courses: list = []
+        while True:
+            feature = curses.wrapper(lambda stdscr: prompt(stdscr, list_features, "Choose feature class:\n", False))
+            if feature == 'EXIT':
+                return
+            elif feature == 'Finish':
+                break
+            elif feature == 'Combinaison':
+                first_class = curses.wrapper(lambda stdscr: prompt(stdscr, list_courses, "Choose first class for combination:\n", False))
+                if first_class == 'EXIT':
+                    break
+                second_class = curses.wrapper(lambda stdscr: prompt(stdscr, list_courses, "Choose second class for combination:\n", False))
+                if second_class == 'EXIT':
+                    break
+                if first_class != second_class:
+                    chosen_courses.append(f"{first_class}/{second_class}")
+                else:
+                    print("The classes chosen must be different")
+                    time.sleep(2)
+                    continue
+            else:
+                chosen_courses.append(feature)
+        if not chosen_courses:
+            print("no courses were chosen")
+        else:
+            print("chosen course")
+            prepare_data(chosen_courses, numeric_df)
 
-			first_class = curses.wrapper(lambda stdscr: prompt(stdscr, list_courses, "Choose first class:\n", False))
-			if first_class == 'EXIT':
-				return
-			second_class = curses.wrapper(lambda stdscr: prompt(stdscr, list_courses, "Choose second class:\n", False))
-			if second_class == 'EXIT':
-				return
-			# Validating classes choices
-			if first_class != second_class:
-				normalized_choice = curses.wrapper(lambda stdscr: prompt(stdscr, ['Yes', 'No'], "Normalized data?\n", False))
-				if normalized_choice == "Yes":
-					normalized_data_flag = True
-				elif normalized_choice == 'EXIT':
-					return
-				break
-			else:
-				print("The classes choised must be different")
-				time.sleep(2)
 
-		execute_plotter_feature(house_classes_data, (first_class, second_class), feature, normalized_data_flag)
+            # print(chosen_courses)
+            # if feature == 'all_scatter_plots_grades':
+            #     generate_all_the_scatter_plots_of_grades(house_classes_data, list_courses)
+            #     exit()
 
-	except ValueError as e:
-		print(e)
-	except CourseNotFound as e:
-		print(e)
-	except FileNotFoundError:
-		print('Failed to read the dataset')
+        #     first_class = curses.wrapper(lambda stdscr: prompt(stdscr, list_courses, "Choose first class:\n", False))
+        #     if first_class == 'EXIT':
+        #         return
+        #     second_class = curses.wrapper(lambda stdscr: prompt(stdscr, list_courses, "Choose second class:\n", False))
+        #     if second_class == 'EXIT':
+        #         return
+        #     # Validating classes choices
+        #     if first_class != second_class:
+        #         normalized_choice = curses.wrapper(lambda stdscr: prompt(stdscr, ['Yes', 'No'], "Normalized data?\n", False))
+        #         if normalized_choice == "Yes":
+        #             normalized_data_flag = True
+        #         elif normalized_choice == 'EXIT':
+        #             return
+        #         break
+        #     else:
+        #         print("The classes choised must be different")
+        #         time.sleep(2)
+
+        # execute_plotter_feature(house_classes_data, (first_class, second_class), feature, normalized_data_flag)
+
+    except ValueError as e:
+        print(e)
+    except CourseNotFound as e:
+        print(e)
+    except FileNotFoundError:
+        print('Failed to read the dataset')
 
 if __name__ == "__main__":
-	main()
+    main()
